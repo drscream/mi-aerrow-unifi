7985e0ffec1166c54167b87806c2651f159eb541
Thomas Merkel <tm@core.io>
2016-07-26 10:15:05 +0200

    dont fuckup the names

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -4,12 +4,12 @@ DDS=zones/$UUID/data
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
 	zfs create ${DDS}/unifi-data || true
-	zfs create ${DDS}/unify-logs || true
-	zfs create ${DDS}/unify-work || true
+	zfs create ${DDS}/unifi-logs || true
+	zfs create ${DDS}/unifi-work || true
 
-	zfs set mountpoint=/opt/local/UniFi/data ${DDS}/unify-data
-	zfs set mountpoint=/opt/local/UniFi/logs ${DDS}/unify-logs
-	zfs set mountpoint=/opt/local/UniFi/work ${DDS}/unify-work
+	zfs set mountpoint=/opt/local/UniFi/data ${DDS}/unifi-data
+	zfs set mountpoint=/opt/local/UniFi/logs ${DDS}/unifi-logs
+	zfs set mountpoint=/opt/local/UniFi/work ${DDS}/unifi-work
 
 	zfs set compression=lz4 ${DDS}
 fi
