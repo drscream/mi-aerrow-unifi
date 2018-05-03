550d76f7ff575ba2099c0af73541b2b1b3daed8f
Thomas Merkel <tm@core.io>
2018-05-03 22:17:40 +0200

    Enable nginx only if nginx_ssl certificate is available

--- copy/var/zoneinit/includes/24-nginx.sh
+++ copy/var/zoneinit/includes/24-nginx.sh
@@ -1,1 +1,4 @@
-svcadm enable svc:/pkgsrc/nginx:default
+if mdata-get nginx_ssl > /dev/null 2>&1; then
+	mdata-get nginx_ssl > /opt/local/etc/nginx/ssl/nginx.pem
+	svcadm enable svc:/pkgsrc/nginx:default
+fi
